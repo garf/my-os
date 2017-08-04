4821464fba4411a4be7b63c2be374d6ecebb571a
Dinar Garipov <garipov.dinar@gmail.com>
2017-08-04 12:42:39 +0200

    Slack -f install

--- scripts/fast-install.sh
+++ scripts/fast-install.sh
@@ -20,8 +20,9 @@ sudo apt -y install yakuake mc git git-core git-gui vim thunderbird curl sublime
 
 mkdir -p tmp
 cd tmp
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.7.0-amd64.deb
-sudo dpkg -i slack-desktop-2.7.0-amd64.deb
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.7.0-amd64.deb -O slack.deb
+sudo dpkg -i slack.deb
+sudo apt -f -y install
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
